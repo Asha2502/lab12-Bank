c444647e84f664ff71c81075912b51ddf44b388f
Asha2502 <159287265+Asha2502@users.noreply.github.com>
2024-05-06 17:29:16 +0300

    Update userList.java

--- src/bankaccount/userList.java
+++ src/bankaccount/userList.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class userList {
     public ArrayList<User> userList = new ArrayList<>();
     public ArrayList init() {
-        userList.add(new User(1, "Мария Лисневская", Arrays.asList(new BankAccount(1, -1000), new BankAccount(5, 7000))));
-        userList.add(new User(2, "Малиновский Матвей",  List.of(new BankAccount(2, -5000))));
-        userList.add(new User(3,"Высоцкий Тимофей", Arrays.asList(new BankAccount(3, 3000), new BankAccount(7, -3000))));
-        userList.add(new User(4, "Войтенков Роман", List.of(new BankAccount(4, 6000))));
-        userList.add(new User(5, "Гриб Иван", Arrays.asList(new BankAccount(8, 5000), new BankAccount(6, -1000))));
+        userList.add(new User(1, "Мария", Arrays.asList(new BankAccount(1, -1000), new BankAccount(5, 7000))));
+        userList.add(new User(2, "Матвей",  List.of(new BankAccount(2, -5000))));
+        userList.add(new User(3,"Тимофей", Arrays.asList(new BankAccount(3, 3000), new BankAccount(7, -3000))));
+        userList.add(new User(4, "Роман", List.of(new BankAccount(4, 6000))));
+        userList.add(new User(5, "Иван", Arrays.asList(new BankAccount(8, 5000), new BankAccount(6, -1000))));
         return userList;
     }
 }
